1a0687a2f25b450a0880d7f17f434eeee9e6ffdf
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-04-21 13:07:16 +0200

    update

--- src/index.js
+++ src/index.js
@@ -8,16 +8,15 @@ const DEBOUNCE_DELAY = 300;
 const ulEl = document.querySelector('.country-list');
 const inputEl = document.querySelector('#search-box');
 
-
-
 inputEl.addEventListener('input', debounce(onInputText, DEBOUNCE_DELAY));
 
 function onInputText(e) {
     const inputedText = e.target.value.trim();
-    fetchCountries(inputedText).then(arrayOfCountries => {
+    fetchCountries(inputedText)
+    .then(arrayOfCountries => {
         console.log(arrayOfCountries);
         if (arrayOfCountries.length > 10) {
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         } else if (arrayOfCountries.length === 1) {
             ulEl.innerHTML = '';
             createMarkupCountry(arrayOfCountries);
@@ -26,18 +25,24 @@ function onInputText(e) {
             markupFlagUndNameCountry(arrayOfCountries);
         }
     })
+    .catch(() => {
+        ulEl.innerHTML = '';
+        Notiflix.Notify.failure('Oops, there is no country with that name.');
+    })
 }
 
 function createMarkupCountry(country) {
     const countryMarkup = country.map(({ name, capital, population, flags, languages }) => {
         return `
-            <li>${name.common}</li>
-            <li>Capital: ${capital}</li>
-            <li>Population: ${population}</li>
-            <li>
-                <img class="country__img" src="${flags.svg}" alt="flag" width="80" height="50">
+        <div class = "country-list-item">
+            <li >
+                <img class="country-img" src="${flags.svg}" alt="flag" width="35" height="20">
+                <span class="country-list-name">${name.common}</span>
             </li>
-            <li>Languages: ${Object.values(languages).join(', ')}</li>
+        </div>
+        <li class ="info-country-list">Capital: <span class="main-info">${capital}</span></li>
+        <li class ="info-country-list">Population: <span class="main-info">${population}</span></li>
+        <li class ="info-country-list">Languages: <span class="main-info">${Object.values(languages).join(', ')}</span></li>
         `
     })
         .join('');
@@ -48,10 +53,12 @@ function createMarkupCountry(country) {
 function markupFlagUndNameCountry(country) {
     const countryList = country.map(({ name, flags }) => {
         return `
-        <li>${name.common}</li>
-        <li>
-            <img class="country__img" src="${flags.svg}" alt="flag" width="80" height="50">
-        </li>
+        <div class = "country-list-item">
+            <li >
+                <img class="country-img" src="${flags.svg}" alt="flag" width="35" height="20">
+                <span class="country-list-name">${name.common}</span>
+            </li>
+        </div>
         `
     }).join('')
     ulEl.insertAdjacentHTML('afterbegin', countryList);
