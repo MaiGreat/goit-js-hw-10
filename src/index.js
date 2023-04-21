1a6252cd9549dbc7e048edbf95c1c3938a71df24
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-04-21 12:19:37 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,40 +1,58 @@
 import './css/styles.css';
-import { fetchCountries }  from './js/fetchCountries'
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
 const ulEl = document.querySelector('.country-list');
-console.log(ulEl);
 const inputEl = document.querySelector('#search-box');
 
 
 
-inputEl.addEventListener('input', onInputText);
+inputEl.addEventListener('input', debounce(onInputText, DEBOUNCE_DELAY));
 
 function onInputText(e) {
-    const inputedText = e.target.value;
-    fetchCountries(inputedText);
-    
-    
-    
-
+    const inputedText = e.target.value.trim();
+    fetchCountries(inputedText).then(arrayOfCountries => {
+        console.log(arrayOfCountries);
+        if (arrayOfCountries.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } else if (arrayOfCountries.length === 1) {
+            ulEl.innerHTML = '';
+            createMarkupCountry(arrayOfCountries);
+        } else {
+            ulEl.innerHTML = '';
+            markupFlagUndNameCountry(arrayOfCountries);
+        }
+    })
 }
 
-function markupCountry(country) {
-    return country.map(({ name, capital, population, flag, languages }) => {
+function createMarkupCountry(country) {
+    const countryMarkup = country.map(({ name, capital, population, flags, languages }) => {
         return `
-            <li>Name of country: '${name}'</li>
-            <li>Capital: '${capital}'</li>
-            <li>Population: '${population}'</li>
-            <li>Flag:
-            <a href="">
-                        <svg>
-                            <use></use>
-                        </svg>
-                    </a>
+            <li>${name.common}</li>
+            <li>Capital: ${capital}</li>
+            <li>Population: ${population}</li>
+            <li>
+                <img class="country__img" src="${flags.svg}" alt="flag" width="80" height="50">
             </li>
-            <li>Languages: '${languages}'</li>
-
+            <li>Languages: ${Object.values(languages).join(', ')}</li>
         `
     })
+        .join('');
+    
+    ulEl.insertAdjacentHTML('afterbegin', countryMarkup);
+}
+
+function markupFlagUndNameCountry(country) {
+    const countryList = country.map(({ name, flags }) => {
+        return `
+        <li>${name.common}</li>
+        <li>
+            <img class="country__img" src="${flags.svg}" alt="flag" width="80" height="50">
+        </li>
+        `
+    }).join('')
+    ulEl.insertAdjacentHTML('afterbegin', countryList);
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,9 @@
 export function fetchCountries(request) {
-   fetch(`https://restcountries.com/v3.1/name/${request}?fields=name,capital,population,flags,languages`)
+    return fetch(`https://restcountries.com/v3.1/name/${request}?fields=name,capital,population,flags,languages`)
        .then(respons => {
-        console.log(respons);
         return respons.json();
         })
-        .then(data => {
-        return data;
-        })
+//         .then(data => {
+//         return data;
+//         })
 };
